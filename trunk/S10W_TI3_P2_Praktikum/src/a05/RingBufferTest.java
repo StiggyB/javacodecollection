512d1a966a0bed22f540c0e5e46e25b915c8f8dc
soeren.rumpf@gmail.com <soeren.rumpf@gmail.com@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2010-11-09 22:04:35 +0000

    #

--- trunk/S10W_TI3_P2_Praktikum/src/a05/RingBufferTest.java
+++ trunk/S10W_TI3_P2_Praktikum/src/a05/RingBufferTest.java
@@ -1,52 +1,49 @@
 package a05;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-/**
- * Praktikum: P1P<br>
- * Semester: SS10<br>
- * Aufgaben-Nr.: 11<br>
- * 
- * Version: V1<br>
- * Aenderungen:
- * 
- * Quellen:
- * 
- * @author Mueller-Pettenpohl, Tell #1989982<br>
- */
+
 public class RingBufferTest {
 
+    @Test
+    public void testRingBuffer() {
+        fail("Not yet implemented");
+    }
 
     @Test
-    public void testFindPalindrom() {
-        TextAnalyser.text.add("Ein Neger mit Gazele zagt im regen nie.");
-        TextAnalyser.findPalindrom();
-        
-        int i = 0;
-        if (i < TextAnalyser.palindroms.size()) {
-            assertEquals("Ein Neger mit Gazele zagt im regen nie.", TextAnalyser.palindroms
-                    .get(i++));
-        }
-
-        assertEquals("Ein Neger mit Gazele zagt im regen nie.", TextAnalyser
-                .checkPalinSen(new StringBuilder("Ein Neger mit Gazele zagt im regen nie.")));
-        assertEquals(
-                "Die Liebe ist Sieger; stets rege ist sie bei Leid.",
-                TextAnalyser
-                        .checkPalinSen(new StringBuilder(
-                                "Die Liebe ist Sieger; stets rege ist sie bei Leid.")));
-        assertEquals("O Genie, der Herr ehre Dein Ego!", TextAnalyser
-                .checkPalinSen(new StringBuilder(
-                        "O Genie, der Herr ehre Dein Ego!")));
-        assertEquals("Trug Tim eine so helle Hose nie mit Gurt?",
-                TextAnalyser.checkPalinSen(new StringBuilder(
-                        "Trug Tim eine so helle Hose nie mit Gurt?")));
+    public void testIsEmpty() {
+        fail("Not yet implemented");
     }
-//  @Test
-     public void testFindAnagram() {
-     assertEquals("HAENSEL UND GRETEL EHRT ENGLANDS EULE",
-     TextAnalyser.findAnagram(/*File file*/));
-     assertEquals("CHAOS ACH SO!", TextAnalyser.findAnagram(/*File file*/));
-     assertEquals("RENTENDISPUT SPINNERDUETT", TextAnalyser.findAnagram(/*File
-     file*/));
-     }
+
+    @Test
+    public void testSize() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsFull() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPeek() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEnqueue() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDequeue() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMain() {
+        fail("Not yet implemented");
+    }
+
 }
