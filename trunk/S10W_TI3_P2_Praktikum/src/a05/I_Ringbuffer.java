2c165caf1eaf51c7af537dfdf3e38c4f2b240cb7
soeren.rumpf@gmail.com <soeren.rumpf@gmail.com@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2010-11-09 15:03:21 +0000

    #

--- trunk/S10W_TI3_P2_Praktikum/src/a05/I_Ringbuffer.java
+++ trunk/S10W_TI3_P2_Praktikum/src/a05/I_Ringbuffer.java
@@ -1,6 +1,13 @@
 package a05;
 
 public interface I_Ringbuffer<Item> {
-    void enqueue( Item item);
-    
+    private Item[] a;            // queue elements
+    private int N = 0;           // number of elements on queue
+    private int first = 0;       // index of first element of queue
+    private int last  = 0;       // index of next available slot
+    public void enqueue( Item item);
+    public void dequeue( Item item);
+    public Item peek ();
+    public boolean isEmpty();
+    public oolean isFull();
 }
