e578e10d9f52a36bf18308f6286163de61d8b8d5
soeren.rumpf@gmail.com <soeren.rumpf@gmail.com@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2010-11-09 21:54:08 +0000

    #

---
+++ trunk/S10W_TI3_P2_Praktikum/src/a05/RingBufferTest.java
@@ -0,0 +1,52 @@
+package a05;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+/**
+ * Praktikum: P1P<br>
+ * Semester: SS10<br>
+ * Aufgaben-Nr.: 11<br>
+ * 
+ * Version: V1<br>
+ * Aenderungen:
+ * 
+ * Quellen:
+ * 
+ * @author Mueller-Pettenpohl, Tell #1989982<br>
+ */
+public class RingBufferTest {
+
+
+    @Test
+    public void testFindPalindrom() {
+        TextAnalyser.text.add("Ein Neger mit Gazele zagt im regen nie.");
+        TextAnalyser.findPalindrom();
+        
+        int i = 0;
+        if (i < TextAnalyser.palindroms.size()) {
+            assertEquals("Ein Neger mit Gazele zagt im regen nie.", TextAnalyser.palindroms
+                    .get(i++));
+        }
+
+        assertEquals("Ein Neger mit Gazele zagt im regen nie.", TextAnalyser
+                .checkPalinSen(new StringBuilder("Ein Neger mit Gazele zagt im regen nie.")));
+        assertEquals(
+                "Die Liebe ist Sieger; stets rege ist sie bei Leid.",
+                TextAnalyser
+                        .checkPalinSen(new StringBuilder(
+                                "Die Liebe ist Sieger; stets rege ist sie bei Leid.")));
+        assertEquals("O Genie, der Herr ehre Dein Ego!", TextAnalyser
+                .checkPalinSen(new StringBuilder(
+                        "O Genie, der Herr ehre Dein Ego!")));
+        assertEquals("Trug Tim eine so helle Hose nie mit Gurt?",
+                TextAnalyser.checkPalinSen(new StringBuilder(
+                        "Trug Tim eine so helle Hose nie mit Gurt?")));
+    }
+//  @Test
+     public void testFindAnagram() {
+     assertEquals("HAENSEL UND GRETEL EHRT ENGLANDS EULE",
+     TextAnalyser.findAnagram(/*File file*/));
+     assertEquals("CHAOS ACH SO!", TextAnalyser.findAnagram(/*File file*/));
+     assertEquals("RENTENDISPUT SPINNERDUETT", TextAnalyser.findAnagram(/*File
+     file*/));
+     }
+}
