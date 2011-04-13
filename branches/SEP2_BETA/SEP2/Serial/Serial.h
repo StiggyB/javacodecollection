b656ad64020660b2e41410c018de7b21cd337787
On.East@gmail.com <On.East@gmail.com@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2011-04-13 14:55:45 +0000

    kommentare in serial.h hinzugefügt für die doku

--- branches/SEP2_BETA/SEP2/Serial/Serial.h
+++ branches/SEP2_BETA/SEP2/Serial/Serial.h
@@ -1,28 +1,55 @@
-/*
- * Serial.h
+/**
+ * Interface for the Hardware Abstraction Layer.
  *
- *  Created on: 12.04.2011
- *      Author: user
+ * SE2 (+ SY and PL) Project SoSe 2011
+ *
+ * Authors: Rico Flaegel,
+ * 			Tell Mueller-Pettenpohl,
+ * 			Torsten Krane,
+ * 			Jan Quenzel
+ *
+ *This will test the serial interface.
+ *depend on the modi. To do a success test, one thread with modi 1 and
+ *the other with modi 0. two threads witch the same modi wont work.
+ *
  */
-
 
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
 #include "Serial.h"
-#include "HAWThread.h"
+#include "../Thread/HAWThread.h"
 
 
 class Serial : public thread::HAWThread{
 public:
-	Serial(int numComPort, int choose, int debug);
+	/**
+	 * Constructor with integrated initialization
+	 * \param numComPort an integer, specifying the Com Port.
+	 * \param modi an integer, 0 for send SYN, get ACK; 1 for get SYN, send ACK
+	 */
+	Serial(int numComPort, int modi, int debug);
 	~Serial();
-	int init(int numComPort, int choose, int debug );
-	int send(void*, int lenByte);
-	int receive(void*, int lenByte);
 
 protected:
-	virtual void execute(void*);
+	/**
+	 * writes on Com Port
+	 * \param data a void pointer, contains the data to send
+	 * \param data an integer, lengths in bytes of data
+	 * \return an integer, 0 for okay, -1 for error.
+	 */
+	int send(void* data, int lenByte);
+	/**
+	 * Reads from com Port
+	 * \param data a void pointer, contains the data container to store incomming data
+	 * \return an integer, 0 for okay -1 for error, -2 for no data received
+	 */
+	int receive(void*, int lenByte);
+	/**
+	 * will start the thread to do his work (send/receive), depend on modi
+	 * \param data a void pointer, in this case it will ignored..no use
+	 */
+	virtual void execute(void* data);
 	virtual void shutdown();
 
 private:
