b0e888d9dbc3d191d13c9c36b2be53abce3a7449
tell12345@web.de <tell12345@web.de@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2011-05-23 11:33:19 +0000

    new branch

--- branches/SEP2_TMP/x86/o/Singleton.d
+++
@@ -1,4 +0,0 @@
-Singleton.o: \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Singleton.cpp \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Singleton.h \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Mutex.h
--- branches/SEP2_TMP/x86/o/Singleton_Template.d
+++
@@ -1,4 +0,0 @@
-Singleton_Template.o: \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Singleton_Template.cpp \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Singleton_Template.h \
- Z:/TI_Labor/QNX650/workspace/SEP2_NEW_BRANCH/Thread/Mutex.h
