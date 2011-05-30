2e1524d1579711d0b2d952833d13e122549897a1
on.east@gmail.com <on.east@gmail.com@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2011-05-30 16:31:45 +0000

    update knöpfe

--- Controller/Sensor.cpp
+++ Controller/Sensor.cpp
@@ -74,7 +74,7 @@ void Sensor::handleNormalMessage() {
 		port = INTERRUPT_D_SERIAL;
 	}
 
-	//cout << "Sensor: some input m.ca: " << r_msg->m.ca << " val: " << (*r_msg).pulse.value.sival_int << endl;
+	cout << "Sensor: some input m.ca: " << r_msg->m.ca << " val: " << (*r_msg).pulse.value.sival_int << endl;
 
 	int val = (*r_msg).pulse.value.sival_int;
 
