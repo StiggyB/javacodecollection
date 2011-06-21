6cfbf1352c30d7ab791d0806a2f5692744d7384c
tell12345@web.de <tell12345@web.de@72d44fe2-11f0-84eb-49d3-ba3949d4c1b1>
2011-06-21 07:31:41 +0000

    sync

--- SEP2_M6/Timer/Timer.cpp
+++ SEP2_M6/Timer/Timer.cpp
@@ -23,9 +23,9 @@ Timer::Timer() {
 }
 
 Timer::~Timer() {
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
-		if( timer_delete( funcp_list[i].timer_id ) != 0){
-			perror( "Timer: cannot delete OS-Timer");
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
+		if (timer_delete(funcp_list[i].timer_id) != 0) {
+			perror("Timer: cannot delete OS-Timer");
 		}//if
 	}//for
 	funcp_list.clear();
@@ -40,7 +40,7 @@ void Timer::execute(void*) {
 			rcvid = MsgReceive(chid, r_msg, sizeof(Message), NULL);
 			handlePulsMessage();
 		}//while
-	}else{
+	} else {
 		perror("Timer: Setting Up failed!");
 		endCommunication();
 	}//if
@@ -57,9 +57,10 @@ int Timer::addTimerFunction(struct IdTOfunction new_element, int ms) {
 	int nano_sec = 0;
 	int sec = 0;
 
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
-		if(funcp_list[i].id == new_element.id){
-			perror("Timer: double id found, this should be unique per getid()!!");
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
+		if (funcp_list[i].id == new_element.id) {
+			perror(
+					"Timer: double id found, this should be unique per getid()!!");
 			return false;
 		}//if
 	}//for
@@ -67,7 +68,7 @@ int Timer::addTimerFunction(struct IdTOfunction new_element, int ms) {
 	new_element.duration_ms = ms;
 
 	new_element.struct_time = getSystemTime_struct();
-//	printf("system time in stopAll_actual_Timer: %i\n", getSystemTime_struct() );
+	//	printf("system time in stopAll_actual_Timer: %i\n", getSystemTime_struct() );
 
 	if (calculateTime(ms, &sec, &nano_sec) == -1) {
 		perror("Timer: cannot calculate seconds and nano seconds");
@@ -78,7 +79,6 @@ int Timer::addTimerFunction(struct IdTOfunction new_element, int ms) {
 	timer.it_value.tv_nsec = nano_sec;
 	timer.it_interval.tv_nsec = 0;
 	timer.it_interval.tv_sec = 0;
-
 
 	SIGEV_PULSE_INIT(&event, coid, SIGEV_PULSE_PRIO_INHERIT, NULL, new_element.id );
 
@@ -103,52 +103,54 @@ int Timer::addTimerFunction(struct IdTOfunction new_element, int ms) {
 	return new_element.id;
 }
 
-
-int Timer::addTimerFunction( CallInterface<CallBackThrower, void>* funcp, int ms, int id){
-    struct IdTOfunction new_element;
-    new_element.funcp.funcp_cbt_void = funcp;
-    new_element.id = id;
-    return (addTimerFunction(new_element, ms));
+int Timer::addTimerFunction(CallInterface<CallBackThrower, void>* funcp,
+		int ms, int id) {
+	struct IdTOfunction new_element;
+	new_element.funcp.funcp_cbt_void = funcp;
+	new_element.id = id;
+	return (addTimerFunction(new_element, ms));
 }
 
-void Timer::handleNormalMessage(){
-	std::cout << "Timer: received a message, but doesn't know what to do with it" << std::endl;
+void Timer::handleNormalMessage() {
+	std::cout
+			<< "Timer: received a message, but doesn't know what to do with it"
+			<< std::endl;
 }
 
-void Timer::handlePulsMessage(){
+void Timer::handlePulsMessage() {
 	//std::cout << "Timer: received a Puls" << std::endl;
 	struct IdTOfunction temp;
 	temp = find_function(r_msg->pulse.value.sival_int);
-	if( temp.id != -1 ){
+	if (temp.id != -1) {
 
 		//printf("Timer: start_execute_duration=%i ", getSystemTime_ms()-temp.systemtime_ms );
 
-		if( temp.funcp.funcp_cbt_void != NULL ){
+		if (temp.funcp.funcp_cbt_void != NULL) {
 			temp.funcp.funcp_cbt_void->call();
-//			std::cout << "Timer: -->call()" << std::endl;
+			//			std::cout << "Timer: -->call()" << std::endl;
 		}//if
 
 
 	} else {
-		perror( "Timer: got pulseMesage for a not known Timer?!");
+		perror("Timer: got pulseMesage for a not known Timer?!");
 	}//if
 }
 
-struct IdTOfunction Timer::find_function(int id){
+struct IdTOfunction Timer::find_function(int id) {
 	struct IdTOfunction result;
 	result.id = -1;
 
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
 
-		if(funcp_list[i].id == id){
+		if (funcp_list[i].id == id) {
 			result = funcp_list[i];
 
-			if( timer_delete( funcp_list[i].timer_id ) == -1){
-				perror( "Timer: cannot delete OS-Timer");
+			if (timer_delete(funcp_list[i].timer_id) == -1) {
+				perror("Timer: cannot delete OS-Timer");
 				return result;
 			}//if
 
-			funcp_list.erase(funcp_list.begin()+i);
+			funcp_list.erase(funcp_list.begin() + i);
 			return result;
 		}//if
 
@@ -156,69 +158,70 @@ struct IdTOfunction Timer::find_function(int id){
 	return result;
 }
 
-int Timer::getnextid(){
-	if(id_counter>100000){
-		perror( "Timer: more than 100000 id's _> set to 100 !!");
+int Timer::getnextid() {
+	if (id_counter > 100000) {
+		perror("Timer: more than 100000 id's _> set to 100 !!");
 		id_counter = 100;
 	}//if
 	return (id_counter++);
 }
 
+int Timer::calculateTime(int ms, int *s, int *ns) {
+	if (ms <= 10000) {
 
-int Timer::calculateTime(int ms, int *s, int *ns){
-    if(ms <= 10000){
+		if (ms >= 1000) {
+			(*s) = ms / 1000;
+			(*ns) = (ms - ((*s) * 1000)) * 1000 * 1000;
+		} else {
+			(*ns) = ms * 1000 * 1000;
+		}//if
 
-    	if(ms >= 1000){
-    		(*s) = ms/1000;
-    		(*ns) = (ms-( (*s)*1000))*1000*1000;
-    	}else{
-    		(*ns) = ms*1000*1000;
-    	}//if
-
-    } else {
-    	perror( "Timer: parameter ms not in range (0 <= ms <= 10000)!");
-    	return -1;
-    }//if
-    return 0;
+	} else {
+		perror("Timer: parameter ms not in range (0 <= ms <= 10000)!");
+		return -1;
+	}//if
+	return 0;
 }
 
-struct timeval Timer::getSystemTime_struct(){
+struct timeval Timer::getSystemTime_struct() {
 	struct timeval act_time;
-	if ( gettimeofday(&act_time, NULL) != 0 ){
+	if (gettimeofday(&act_time, NULL) != 0) {
 		perror("Timer: getSystemTime_ms failed - gettimeofday!=0");
 		return act_time;
 	}
 	return act_time;
 }
 
-int Timer::stopAll_actual_Timer(){
+int Timer::stopAll_actual_Timer() {
 	locker.lock();
-	long diff = 0;
 	bool unstoppablefound = false;
 	bool notfound = true;
 	struct timeval temp = getSystemTime_struct();
 	long diff_sec;
 	long diff_usec;
 
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
 		notfound = true;
 		unstoppablefound = false;
 
-		for(unsigned int j = 0; j<unstoppable_funcp_list.size() && notfound; j++){
-			if(funcp_list[i].funcp.funcp_cbt_void == unstoppable_funcp_list[j]){
+		for (unsigned int j = 0; j < unstoppable_funcp_list.size() && notfound; j++) {
+			if (funcp_list[i].funcp.funcp_cbt_void == unstoppable_funcp_list[j]) {
 				unstoppablefound = true;
 				//cout << "unstoppablefound found" << endl;
 				notfound = false;
 			}//if
 		}//for
 
-		if(!unstoppablefound){
+		if (!unstoppablefound) {
+			if (funcp_list[i].timer_id != -1) {
 
-			if( timer_delete( funcp_list[i].timer_id ) == -1){
-				perror( "Timer: cannot delete OS-Timer in Timer_stopAllTimer()");
-				locker.unlock();
-				return -1;
-			}//if
+				if (timer_delete(funcp_list[i].timer_id) == -1) {
+					perror(
+							"Timer: cannot delete OS-Timer in Timer_stopAllTimer()");
+					locker.unlock();
+					return -1;
+				}//if
+			}
 			funcp_list[i].timer_id = -1;
 
 			temp = getSystemTime_struct();
@@ -226,10 +229,11 @@ int Timer::stopAll_actual_Timer(){
 			diff_sec = temp.tv_sec - funcp_list[i].struct_time.tv_sec;
 			diff_usec = temp.tv_usec - funcp_list[i].struct_time.tv_usec;
 
-//			printf("Timer: diffsec:%i diff_usec:%i\n", diff_sec, diff_usec);
-//			printf("Timer: diff ms %ld\n",  (((diff_sec)*1000)  +  (long)(diff_usec/1000.0)) );
+			//			printf("Timer: diffsec:%i diff_usec:%i\n", diff_sec, diff_usec);
+			//			printf("Timer: diff ms %ld\n",  (((diff_sec)*1000)  +  (long)(diff_usec/1000.0)) );
 
-			funcp_list[i].duration_ms = funcp_list[i].duration_ms - (((diff_sec)*1000)  +  (long)(diff_usec/1000.0));
+			funcp_list[i].duration_ms = funcp_list[i].duration_ms
+					- (((diff_sec) * 1000) + (long) (diff_usec / 1000.0));
 
 		}//for
 
@@ -239,7 +243,7 @@ int Timer::stopAll_actual_Timer(){
 	return 0;
 }
 
-int Timer::startAllTimer(){
+int Timer::startAllTimer() {
 	locker.lock();
 	int sec = 0;
 	int nano = 0;
@@ -247,8 +251,8 @@ int Timer::startAllTimer(){
 	struct sigevent event;
 	struct itimerspec timer;
 
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
-		if( funcp_list[i].timer_id == -1 ){//a not stopped timer
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
+		if (funcp_list[i].timer_id == -1) {//a not stopped timer
 
 			calculateTime(funcp_list[i].duration_ms, &sec, &nano);
 
@@ -282,31 +286,29 @@ int Timer::startAllTimer(){
 	return 0;
 }
 
+int Timer::deleteTimer(int id) {
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
+		if (funcp_list[i].id == id && funcp_list[i].timer_id != -1) { //
 
-int Timer::deleteTimer(int id){
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
-		if( funcp_list[i].id == id && funcp_list[i].timer_id != -1 ){ //
-
-			if( timer_delete( funcp_list[i].timer_id ) == -1){
-				perror( "Timer: cannot delete OS-Timer in stopTimerbyId()");
+			if (timer_delete(funcp_list[i].timer_id) == -1) {
+				perror("Timer: cannot delete OS-Timer in stopTimerbyId()");
 				return -1;
 			}//if
 
-//			std::cout << "Timer: stopTimerbyId has delete a Timer - ID: " << funcp_list[i].timer_id << std::endl;
-			funcp_list.erase( funcp_list.begin()+i );
+			//			std::cout << "Timer: stopTimerbyId has delete a Timer - ID: " << funcp_list[i].timer_id << std::endl;
+			funcp_list.erase(funcp_list.begin() + i);
 		}//if
 
 	}//for
 	return 0;
 }
 
-
-int Timer::deleteAllTimer(){
+int Timer::deleteAllTimer() {
 	locker.lock();
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
 
-		if( timer_delete( funcp_list[i].timer_id ) == -1){
-			perror( "Timer: cannot delete OS-Timer in deleteAllTimer()");
+		if (timer_delete(funcp_list[i].timer_id) == -1) {
+			perror("Timer: cannot delete OS-Timer in deleteAllTimer()");
 			locker.unlock();
 			return -1;
 		}//if
@@ -318,15 +320,14 @@ int Timer::deleteAllTimer(){
 	return 0;
 }
 
-int Timer::addUnstoppableFunction( CallInterface<CallBackThrower,void>* funcp ){
+int Timer::addUnstoppableFunction(CallInterface<CallBackThrower, void>* funcp) {
 	unstoppable_funcp_list.push_back(funcp);
 	return 0;
 }
 
-
-bool Timer::existTimer(int id){
-	for(unsigned int i = 0; i<funcp_list.size(); i++){
-		if(funcp_list[i].id == id){
+bool Timer::existTimer(int id) {
+	for (unsigned int i = 0; i < funcp_list.size(); i++) {
+		if (funcp_list[i].id == id) {
 			return true;
 		}
 	}
